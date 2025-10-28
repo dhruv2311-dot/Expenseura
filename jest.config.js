54c77b9ca82ef98d5b0ed633e8f0ca8e8623fbca
Dhruv2311 <dhruv.sonagra.cg@gmail.com>
2025-10-28 17:57:22 +0530

    add packages

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+export default {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.js'
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/main.jsx',
+    '!src/**/*.test.{js,jsx}'
+  ]
+}
